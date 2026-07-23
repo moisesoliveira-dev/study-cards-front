8f817b3d8aa100032b3e23de906854e872e8ee65
moisesoliveira-dev <moi.oli.dua.sei@gmail.com>
2026-07-22 21:08:40 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'study-cards-front',
+  webDir: 'dist'
+};
+
+export default config;
